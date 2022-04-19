02bbccdd537529095c037a9550cc53b84c5f3a94
TiankaiHang <seuhtk98@gmail.com>
2022-04-20 00:42:45 +0800

    apt to apt-get

--- install_mitsuba.sh
+++ install_mitsuba.sh
@@ -1,9 +1,9 @@
 # ref: https://mitsuba2.readthedocs.io/en/latest/src/getting_started/compiling.html
-sudo apt install -y clang-9 libc++-9-dev libc++abi-9-dev cmake ninja-build
-sudo apt install -y libz-dev libpng-dev libjpeg-dev libxrandr-dev libxinerama-dev libxcursor-dev
-sudo apt install -y python3-dev python3-distutils python3-setuptools
-sudo apt install -y python3-pytest python3-pytest-xdist python3-numpy
-sudo apt install -y python3-sphinx python3-guzzle-sphinx-theme python3-sphinxcontrib.bibtex
+sudo apt-get install -y clang-9 libc++-9-dev libc++abi-9-dev cmake ninja-build
+sudo apt-get install -y libz-dev libpng-dev libjpeg-dev libxrandr-dev libxinerama-dev libxcursor-dev
+sudo apt-get install -y python3-dev python3-distutils python3-setuptools
+sudo apt-get install -y python3-pytest python3-pytest-xdist python3-numpy
+sudo apt-get install -y python3-sphinx python3-guzzle-sphinx-theme python3-sphinxcontrib.bibtex
 
 export CC=clang-9
 export CXX=clang++-9
