1600cd4128f71ec744353ee37225e51a0a616618
TiankaiHang <seuhtk98@gmail.com>
2022-05-11 09:55:00 +0800

    up

--- pointflow_fig_colorful.py
+++ pointflow_fig_colorful.py
@@ -62,8 +62,9 @@ def convert_exr_to_jpg(exr_file, jpg_file):
 
 
 def standardize_bbox(pcl, points_per_object=-1):
-    points_per_object = points_per_object \
+    points_per_object = min(pcl.shape[0], points_per_object) \
         if points_per_object > 0 else pcl.shape[0]
+    
     pt_indices = np.random.choice(pcl.shape[0], points_per_object, replace=False)
     np.random.shuffle(pt_indices)
     pcl = pcl[pt_indices] # n by 3
